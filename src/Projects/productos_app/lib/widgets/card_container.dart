1cefae330e1b506b6dfaf9119ebd4876c8013f95
Felipe <u202110902@upc.edu.pe>
2023-07-13 14:26:25 -0500

    login forms validate key http simulator loading

---
+++ src/Projects/productos_app/lib/widgets/card_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CardContainer extends StatelessWidget {
+  const CardContainer({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        decoration: _createCardShape(),
+        child: child,
+      ),
+    );
+  }
+
+  BoxDecoration _createCardShape() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 15,
+              offset: Offset(4, 4),
+            ),
+          ]);
+}
--- src/Projects/productos_app/lib/widgets/widgets.dart
+++ src/Projects/productos_app/lib/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'package:productos_app/widgets/auth_background.dart';
+export 'package:productos_app/widgets/card_container.dart';
